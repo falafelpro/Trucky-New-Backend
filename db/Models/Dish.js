dfd63629c623ad34dc0edade67c5da7cea0de441
falafelpro <47983933+falafelpro@users.noreply.github.com>
2021-12-08 21:50:03 +0300

    Dish M+R+C + user model update

---
+++ db/Models/Dish.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const mongooseSlugPlugin = require("mongoose-slug-plugin");
+const DishSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  image: String,
+  slug: String,
+  type: {
+    type: String,
+    enum: ["Starter", "Main Course", "Side", "Desert", "Drink", null],
+  },
+  owner: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  description: String,
+  price: {
+    type: Number,
+  },
+});
+DishSchema.plugin(mongooseSlugPlugin, { tmpl: "<%=name%>" });
+module.exports = mongoose.model("Dish", DishSchema);
